78fe223575e15e0e5556bb1ffe40a27525b43fdd
braycoeYT <59518485+braycoeYT@users.noreply.github.com>
2020-08-27 08:14:53 -0500

    Add files via upload

---
+++ PufferfishSpike.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace ZylonFanExpansion.Projectiles.Pufferfish
+{
+	public class PufferfishSpike : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Pufferfish Spike");
+		}
+		public override void SetDefaults() {
+			projectile.width = 16;
+			projectile.height = 16;
+			projectile.aiStyle = 1;
+			projectile.friendly = true;
+			projectile.ranged = true;
+			projectile.penetrate = 1;
+		}
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit) {
+			target.AddBuff(BuffID.Poisoned, 60 * Main.rand.Next(3, 6), false);
+		}
+		public override void OnHitPvp(Player target, int damage, bool crit) {
+			target.AddBuff(BuffID.Poisoned, 60 * Main.rand.Next(3, 6), false);
+		}
+	}
+}
